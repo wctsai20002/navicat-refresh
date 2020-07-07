293255569ac44aebe4efd525640ed88ae374c3f8
tweichuan <wctsai20002@gmai.com>
2020-07-07 17:59:32 +0800

    Delete Info

--- navicat-refresh.bat
+++ navicat-refresh.bat
@@ -1,1 +1,2 @@
-reg delete "HKEY_CURRENT_USER\Software\PremiumSoft\NavicatPremium\Registration15XCT" /va
+reg delete "HKEY_CURRENT_USER\Software\PremiumSoft\NavicatPremium\Registration15XCT" /va /f
+reg delete "HKEY_CURRENT_USER\Software\Classes\CLSID\{C655BB37-4538-B9E7-0CB3-79F82FDB1F76}\Info" /va /f
