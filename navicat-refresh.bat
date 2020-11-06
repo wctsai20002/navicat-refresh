9e23be6cca1baae14c7a7603ff81c194e8917587
tweichuan <wctsai20002@gmail.com>
2020-11-06 12:49:54 +0800

    Add echo

--- navicat-refresh.bat
+++ navicat-refresh.bat
@@ -1,7 +1,18 @@
 @echo off
 
+echo Delete HKEY_CURRENT_USER\Software\PremiumSoft\NavicatPremium\Registration15XCT
+echo waiting......
 reg delete "HKEY_CURRENT_USER\Software\PremiumSoft\NavicatPremium\Registration15XCT" /va /f
+echo.
+
+echo Delete Info folder under HKEY_CURRENT_USER\Software\Classes\CLSID
+echo waiting......
 
 for /f %%i in ('"REG QUERY "HKEY_CURRENT_USER\Software\Classes\CLSID" /s | findstr /E Info"') do (
     reg delete %%i /va /f
 )
+
+echo.
+echo Finish
+
+pause
