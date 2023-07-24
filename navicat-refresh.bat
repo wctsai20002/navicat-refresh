caa5e5902d8be6a93567d5936411357de7828148
Abel Ardhana S <42704620+abela-a@users.noreply.github.com>
2023-07-24 15:04:38 +0800

    fix for version 16.2.5
    original source code from @lyc8503 in issue #4

--- navicat-refresh.bat
+++ navicat-refresh.bat
@@ -1,14 +1,23 @@
 @echo off
 
+echo Delete HKEY_CURRENT_USER\Software\PremiumSoft\NavicatPremium\Update
+reg delete HKEY_CURRENT_USER\Software\PremiumSoft\NavicatPremium\Update /f
+echo.
+
 echo Delete HKEY_CURRENT_USER\Software\PremiumSoft\NavicatPremium\Registration[version and language]
 for /f %%i in ('"REG QUERY "HKEY_CURRENT_USER\Software\PremiumSoft\NavicatPremium" /s | findstr /L Registration"') do (
     reg delete %%i /va /f
 )
 echo.
 
-echo Delete Info folder under HKEY_CURRENT_USER\Software\Classes\CLSID
-for /f %%i in ('"REG QUERY "HKEY_CURRENT_USER\Software\Classes\CLSID" /s | findstr /E Info"') do (
-    reg delete %%i /va /f
+echo Delete Info and ShellFolder under HKEY_CURRENT_USER\Software\Classes\CLSID
+for /f "tokens=*" %%a in ('reg query "HKEY_CURRENT_USER\Software\Classes\CLSID"') do (
+  for /f "tokens=*" %%l in ('reg query "%%a" /f "Info" /s /e ^| findstr /i "Info"') do (
+    reg delete %%a /f
+  )
+  for /f "tokens=*" %%l in ('reg query "%%a" /f "ShellFolder" /s /e ^| findstr /i "ShellFolder"') do (
+    reg delete %%a /f
+  )
 )
 echo.
 
